72259640feea761e5a9fcb7528079177f6e3abd6
Michael Enger <michael.enger@pearson.com>
2014-09-22 12:20:09 +0200

    Add option to delete test data

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var Q = require('q'),
     neo4j = require('node-neo4j'),
+    program = require('commander'),
     db = new neo4j('http://192.168.33.10:7474'),
-    data = require('./data.json'),
+    testData = require('./data.json'),
     mapping = {},
     nodePromises = [],
     relationships = [],
@@ -11,6 +12,26 @@ var Q = require('q'),
 
     divider = function () {
         console.log('-----------------------------');
+    },
+
+    /**
+     * Delete all data from the db.
+     *
+     * @return {object} Promise
+     */
+    deleteAllData = function () {
+        var defer = Q.nbind(db.cypherQuery, db);
+
+        return defer('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+            .then(function (results) {
+                console.log('Deleted all data');
+                divider();
+            });
+    },
+
+    errorHandler = function (err) {
+        console.error('Error saving new node to database:', err);
+        divider();
     },
 
     onSuccessfulNodesWrite = function (nodes) {
@@ -54,11 +75,6 @@ var Q = require('q'),
         divider();
     },
 
-    errorHandler = function (err) {
-        console.error('Error saving new node to database:', err);
-        divider();
-    },
-
     saveNode = function (node, type) {
         var saveNodeToDb = Q.nbind(db.insertNode, db);
         return saveNodeToDb(node, [type])
@@ -70,12 +86,66 @@ var Q = require('q'),
                 mapping[type][node.id] = node._id;
                 return Q(node);
             });
-    }
+    },
 
     saveRelationship = function (from, to, type) {
         var saveRelToDb = Q.nbind(db.insertRelationship, db);
         return saveRelToDb(from, to, type, {});
+    }
+
+    writeTestData = function (data) {
+        for (type in data) {
+            for (i = 0; i < data[type].length; i++) {
+                entity = data[type][i];
+
+                switch (type) {
+                    case 'Group':
+                        nodePromises.push(saveNode({
+                            id: entity.id,
+                            name: entity.name
+                        }, type));
+
+                        for (j = 0; j < entity.users.length; j++) {
+                            relationships.push({
+                                type: 'MEMBER',
+                                from: entity.users[j],
+                                to: entity.id
+                            });
+                        }
+
+                        break;
+
+                    case 'School':
+                        nodePromises.push(saveNode({
+                            id: entity.id,
+                            name: entity.name
+                        }, type));
+                        break;
+
+                    case 'User':
+                        nodePromises.push(saveNode({
+                            id: entity.id,
+                            username: entity.username
+                        }, type));
+
+                        relationships.push({
+                            type: entity.role === 'teacher' ? 'TEACHER' : 'STUDENT',
+                            from: entity.id,
+                            to: entity.schoolId
+                        });
+
+                        break;
+                }
+            }
+        }
+
+        Q.all(nodePromises).done(onSuccessfulNodesWrite, errorHandler);
     };
+
+
+program
+    .option('-d, --delete', 'Delete all data before inserting test data')
+    .parse(process.argv);
 
 // ------------------------------------------------------------------
 divider();
@@ -83,49 +153,11 @@ console.log('Running neo4j experiment');
 divider();
 // ------------------------------------------------------------------
 
-for (type in data) {
-    for (i = 0; i < data[type].length; i++) {
-        entity = data[type][i];
-
-        switch (type) {
-            case 'Group':
-                nodePromises.push(saveNode({
-                    id: entity.id,
-                    name: entity.name
-                }, type));
-
-                for (j = 0; j < entity.users.length; j++) {
-                    relationships.push({
-                        type: 'MEMBER',
-                        from: entity.users[j],
-                        to: entity.id
-                    });
-                }
-
-                break;
-
-            case 'School':
-                nodePromises.push(saveNode({
-                    id: entity.id,
-                    name: entity.name
-                }, type));
-                break;
-
-            case 'User':
-                nodePromises.push(saveNode({
-                    id: entity.id,
-                    username: entity.username
-                }, type));
-
-                relationships.push({
-                    type: entity.role === 'teacher' ? 'TEACHER' : 'STUDENT',
-                    from: entity.id,
-                    to: entity.schoolId
-                });
-
-                break;
-        }
-    }
+if (program.delete) {
+    deleteAllData()
+        .done(function () {
+            writeTestData(testData);
+        }, errorHandler);
+} else {
+    writeTestData(testData);
 }
-
-Q.all(nodePromises).done(onSuccessfulNodesWrite, errorHandler);
