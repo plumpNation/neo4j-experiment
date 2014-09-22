0afe9319fd0a1f8b0192581e763d65b78ff27882
Michael Enger <michael.enger@pearson.com>
2014-09-22 12:04:46 +0200

    Add test data to the db

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
 var Q = require('q'),
-    neo4j = require('neo4j'),
-    db = new neo4j.GraphDatabase('http://localhost:7474'),
+    neo4j = require('node-neo4j'),
+    db = new neo4j('http://192.168.33.10:7474'),
+    data = require('./data.json'),
+    mapping = {},
+    nodePromises = [],
+    relationships = [],
+    relationshipPromises = [],
+    entity,
+    i, j,
 
     divider = function () {
-      console.log('-----------------------------');
+        console.log('-----------------------------');
     },
 
-    onSuccessfulWrite = function (node) {
-      console.log('Node saved to database with id:', node.id);
-      divider();
+    onSuccessfulNodesWrite = function (nodes) {
+        nodes = nodes.map(function (n) {
+            return n._id;
+        });
+
+        console.log('Nodes saved to database:', nodes);
+        divider();
+
+        for (i = 0; i < relationships.length; i++) {
+            switch (relationships[i].type) {
+                case 'MEMBER':
+                    relationshipPromises.push(saveRelationship(
+                        mapping['User'][relationships[i].from],
+                        mapping['Group'][relationships[i].to],
+                        relationships[i].type
+                    ));
+                    break;
+
+                case 'STUDENT':
+                case 'TEACHER':
+                    relationshipPromises.push(saveRelationship(
+                        mapping['User'][relationships[i].from],
+                        mapping['School'][relationships[i].to],
+                        relationships[i].type
+                    ));
+                    break;
+            }
+        }
+
+        Q.all(relationshipPromises).done(onSuccessfulRelationshipsWrite, errorHandler);
+    },
+
+    onSuccessfulRelationshipsWrite = function (rels) {
+        rels = rels.map(function (r) {
+            return r._type;
+        });
+
+        console.log('Relationships saved to database:', rels);
+        divider();
     },
 
     errorHandler = function (err) {
-      console.error('Error saving new node to database:', err);
-      divider();
+        console.error('Error saving new node to database:', err);
+        divider();
+    },
+
+    saveNode = function (node, type) {
+        var saveNodeToDb = Q.nbind(db.insertNode, db);
+        return saveNodeToDb(node, [type])
+            .then(function (node) {
+                if (!mapping[type]) {
+                    mapping[type] = {};
+                }
+
+                mapping[type][node.id] = node._id;
+                return Q(node);
+            });
+    }
+
+    saveRelationship = function (from, to, type) {
+        var saveRelToDb = Q.nbind(db.insertRelationship, db);
+        return saveRelToDb(from, to, type, {});
     };
 
 // ------------------------------------------------------------------
@@ -22,19 +83,49 @@ console.log('Running neo4j experiment');
 divider();
 // ------------------------------------------------------------------
 
+for (type in data) {
+    for (i = 0; i < data[type].length; i++) {
+        entity = data[type][i];
 
-// Create our first node
+        switch (type) {
+            case 'Group':
+                nodePromises.push(saveNode({
+                    id: entity.id,
+                    name: entity.name
+                }, type));
 
-// instantaneous response, but not the persisted data
-var node = db.createNode({hello: 'world'});
-console.log(node);
+                for (j = 0; j < entity.users.length; j++) {
+                    relationships.push({
+                        type: 'MEMBER',
+                        from: entity.users[j],
+                        to: entity.id
+                    });
+                }
 
-divider();
-// ------------------------------------------------------------------
+                break;
 
-// https://github.com/kriskowal/q/wiki/API-Reference
-// nbind will allow you to use q.js promise syntax instead of the nodejs
-// callback pattern i.e. function (err, data) {}
-// It is well known not to be as performant as a plain callback.
-var saveNodeToDb = Q.nbind(node.save, node);
-saveNodeToDb().done(onSuccessfulWrite, errorHandler);
+            case 'School':
+                nodePromises.push(saveNode({
+                    id: entity.id,
+                    name: entity.name
+                }, type));
+                break;
+
+            case 'User':
+                nodePromises.push(saveNode({
+                    id: entity.id,
+                    username: entity.username
+                }, type));
+
+                relationships.push({
+                    type: entity.role === 'teacher' ? 'TEACHER' : 'STUDENT',
+                    from: entity.id,
+                    to: entity.schoolId
+                });
+
+                break;
+        }
+    }
+}
+
+Q.all(nodePromises).done(onSuccessfulNodesWrite, errorHandler);
