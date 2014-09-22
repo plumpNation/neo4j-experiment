2a7ef5f4d46330a0634b98548dc52152cbcd921e
Michael Enger <michael.enger@pearson.com>
2014-09-22 14:03:21 +0200

    Add means to randomly generate test data

--- app.js
+++ app.js
@@ -1,14 +1,77 @@
+'use strict';
+
 var Q = require('q'),
     neo4j = require('node-neo4j'), // the "normal" neo4j package didn't support labels
     program = require('commander'),
+    uuid = require('uuid'),
     db = new neo4j('http://192.168.33.10:7474'),
-    testData = require('./data.json'),
     mapping = {},
-    nodePromises = [],
-    relationships = [],
-    relationshipPromises = [],
-    entity,
-    i, j,
+    testData,
+
+    createRandomData = function () {
+        var userCount = randomInt(100, 200),
+            teacherCount = Math.ceil(userCount / randomInt(15, 20)),
+            groupCount = randomInt(2, 3),
+            studentsPerGroup = Math.floor(userCount / (teacherCount * groupCount)) - randomInt(0, 2),
+            data = {},
+            school = {
+                id: uuid.v4(),
+                name: 'Test School'
+            },
+            temp,
+            temp2,
+            groupNum,
+            i, j, k;
+
+        data.School = [school];
+        data.User = [];
+        data.Group = [];
+
+        for (i = 0; i < userCount; i++) {
+            temp = {
+                id: uuid.v4(),
+                username: 'Student ' + i,
+                schoolId: school.id,
+                role: 'student'
+            };
+            data.User.push(temp);
+        }
+
+        for (i = 0; i < teacherCount; i++) {
+            temp = {
+                id: uuid.v4(),
+                username: 'Teacher ' + i,
+                schoolId: school.id,
+                role: 'teacher'
+            };
+            data.User.push(temp);
+
+            for (j = 0; j < groupCount; j++) {
+                groupNum = (i * groupCount) + j;
+                temp2 = {
+                    id: uuid.v4(),
+                    name: 'Group ' + groupNum,
+                    users: [
+                        temp.id
+                    ]
+                };
+
+                for (k = 0; k < studentsPerGroup; k++) {
+                    temp2.users.push(data.User[k + (studentsPerGroup * groupNum)].id);
+                }
+
+                data.Group.push(temp2);
+            }
+        }
+
+        console.log('Created randomized data for: ' +
+            userCount + ' users, ' +
+            teacherCount + ' teachers, and ' +
+            (teacherCount * groupCount) + ' groups');
+        divider();
+
+        return data;
+    },
 
     /**
      * Print out a nice divider.
@@ -54,29 +117,6 @@ var Q = require('q'),
 
         console.log('Nodes saved to database:', nodes);
         divider();
-
-        for (i = 0; i < relationships.length; i++) {
-            switch (relationships[i].type) {
-                case 'MEMBER':
-                    relationshipPromises.push(saveRelationship(
-                        mapping['User'][relationships[i].from],
-                        mapping['Group'][relationships[i].to],
-                        relationships[i].type
-                    ));
-                    break;
-
-                case 'STUDENT':
-                case 'TEACHER':
-                    relationshipPromises.push(saveRelationship(
-                        mapping['User'][relationships[i].from],
-                        mapping['School'][relationships[i].to],
-                        relationships[i].type
-                    ));
-                    break;
-            }
-        }
-
-        Q.all(relationshipPromises).done(onSuccessfulRelationshipsWrite, errorHandler);
     },
 
     /**
@@ -91,6 +131,21 @@ var Q = require('q'),
 
         console.log('Relationships saved to database:', rels);
         divider();
+
+        if (program.index) {
+            createIndex(program.index);
+        }
+    },
+
+    /**
+     * Get a random integer between two values (inclusive).
+     *
+     * @param {number} min Minimum value (inclusive)
+     * @param {number} max Maximum value (inclusive)
+     * @return {number}
+     */
+    randomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
     },
 
     /**
@@ -124,21 +179,65 @@ var Q = require('q'),
     saveRelationship = function (from, to, type) {
         var saveRelToDb = Q.nbind(db.insertRelationship, db);
         return saveRelToDb(from, to, type, {});
-    }
+    },
+
+    /**
+     * Write the relationships to the db.
+     *
+     * @param {array} relationships Relationships to write
+     * @return {function}
+     */
+    writeRelationships = function (relationships) {
+        var promises = [],
+            i;
+
+        return function () {
+            for (i = 0; i < relationships.length; i++) {
+                switch (relationships[i].type) {
+                    case 'MEMBER':
+                        promises.push(saveRelationship(
+                            mapping['User'][relationships[i].from],
+                            mapping['Group'][relationships[i].to],
+                            relationships[i].type
+                        ));
+                        break;
+
+                    case 'STUDENT':
+                    case 'TEACHER':
+                        promises.push(saveRelationship(
+                            mapping['User'][relationships[i].from],
+                            mapping['School'][relationships[i].to],
+                            relationships[i].type
+                        ));
+                        break;
+                }
+            }
+
+            return Q.all(promises).then(onSuccessfulRelationshipsWrite);
+        }
+    },
 
     /**
      * Write test data to the db.
      *
      * @param {object} data Data to write
+     * @return {object} Promise
      */
     writeTestData = function (data) {
+        var promises = [],
+            relationships = [],
+            type,
+            entity,
+            i,
+            j;
+
         for (type in data) {
             for (i = 0; i < data[type].length; i++) {
                 entity = data[type][i];
 
                 switch (type) {
                     case 'Group':
-                        nodePromises.push(saveNode({
+                        promises.push(saveNode({
                             id: entity.id,
                             name: entity.name
                         }, type));
@@ -154,14 +253,14 @@ var Q = require('q'),
                         break;
 
                     case 'School':
-                        nodePromises.push(saveNode({
+                        promises.push(saveNode({
                             id: entity.id,
                             name: entity.name
                         }, type));
                         break;
 
                     case 'User':
-                        nodePromises.push(saveNode({
+                        promises.push(saveNode({
                             id: entity.id,
                             username: entity.username
                         }, type));
@@ -177,12 +276,15 @@ var Q = require('q'),
             }
         }
 
-        Q.all(nodePromises).done(onSuccessfulNodesWrite, errorHandler);
+        return Q.all(promises)
+            .then(onSuccessfulNodesWrite)
+            .then(writeRelationships(relationships));
     };
 
 // Parse the CLI arguments
 program
     .option('-d, --delete', 'Delete all data before inserting test data')
+    .option('-r, --randomize', 'Use randomly generated data')
     .parse(process.argv);
 
 // ------------------------------------------------------------------
@@ -191,11 +293,16 @@ console.log('Running neo4j experiment');
 divider();
 // ------------------------------------------------------------------
 
-if (program.delete) {
-    deleteAllData()
-        .done(function () {
-            writeTestData(testData);
-        }, errorHandler);
+if (program.randomize) {
+    testData = createRandomData();
 } else {
-    writeTestData(testData);
+    testData = require('./data.json');
+}
+
+if (program.delete) {
+    deleteAllData().done(function () {
+        writeTestData(testData);
+    }, errorHandler);
+} else {
+    writeTestData(testData).done(function () {}, errorHandler);
 }
